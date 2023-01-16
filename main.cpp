00c5f5645b4c60fee3d1d7398cea94c925a448ef
mohsen <peykanrom@gmail.com>
2023-01-16 10:41:42 +0330

    Add Description

--- main.cpp
+++ main.cpp
@@ -1,3 +1,15 @@
+/* This is Gobblet-Gobblers game !
+  If u want to win you have to line up 3 of your Gobblers in a row to win !!
+  Each player have 6 pieces called "Gobblers"
+  2 Small Gobblers
+  2 Medium Gobblers
+  2 Big Gobblers
+  U can put ur Gobblers on an empty space or over a small Gobbler
+  and beside that u can move your Gobbler on the board !
+
+                      HAVE FUN !!!!
+*/
+
 #include <iostream>
 using namespace std;
 int main() {
